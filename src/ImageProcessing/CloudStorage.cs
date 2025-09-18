09f96efb588edd01d435b2792fae572d60da96b5
claytonphenderson <claytonphenderson@gmail.com>
2025-09-17 21:55:05 -0500

    rearranging a bit

---
+++ src/ImageProcessing/CloudStorage.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Azure.Identity;
+using Azure.Storage.Files.DataLake;
+
+namespace ImageProcessing;
+
+public static class CloudStorage
+{
+    public static async Task UploadImage(DataLakeFileSystemClient fileSystemClient, string fileName, FileStream fs, Dictionary<string, string>? metadata)
+    {
+        // create the new file or skip if it already exists
+        var fileClient = fileSystemClient.GetFileClient(fileName);
+        if (Environment.GetEnvironmentVariable("OVERWRITE_ON") != "true")
+        {
+            if (await fileClient.ExistsAsync())
+            {
+                Console.WriteLine($"File {fileName} exists already. Skipping.");
+                return;
+            }
+        }
+
+        // Upload bytes and set metadata fields
+        await fileClient.UploadAsync(fs);
+
+        if (metadata is not null)
+        {
+            await fileClient.SetMetadataAsync(metadata);
+        }
+    }
+
+}
---
+++ src/ImageProcessing/Utils.cs
@@ -0,0 +1,60 @@
+using System.Security.Cryptography;
+using System.Text.Encodings.Web;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using TagLib.Image;
+
+namespace ImageProcessing;
+
+public static class Utils
+{
+    public static DateTime GetExifDate(string path)
+    {
+        // Read exif data
+        var directories = ImageMetadataReader.ReadMetadata(path);
+        var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+
+        // Read original image capture date
+        var imageDate = DateTime.Now;
+        if (subIfdDirectory != null && subIfdDirectory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out DateTime dateTime))
+        {
+            imageDate = dateTime;
+        }
+
+        return imageDate;
+    }
+
+    public static ImageOrientation GetExifOrientation(string path)
+    {
+        // Read exif data
+        var directories = ImageMetadataReader.ReadMetadata(path);
+        var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+
+        // Read orientation of image
+        var imageOrientation = ImageOrientation.None;
+        if (subIfdDirectory != null && subIfdDirectory.TryGetInt32(ExifDirectoryBase.TagOrientation, out int orientation))
+        {
+            imageOrientation = (ImageOrientation)orientation;
+        }
+
+        return imageOrientation;
+    }
+
+    /// <summary>
+    /// Uses MD5 to hash the image bytes.  Useful for deduplication and id.
+    /// </summary>
+    /// <param name="fs"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static string GetImageHash(FileStream fs)
+    {
+        var hashedResult = "";
+        using (var md5 = MD5.Create())
+        {
+            hashedResult = md5.ComputeHash(fs).ToString() ?? throw new Exception("Could not hash image");
+        }
+
+        fs.Position = 0; //rewind the stream for future use
+        return UrlEncoder.Create().Encode(hashedResult);
+    }
+}
